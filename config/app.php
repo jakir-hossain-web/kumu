a8b30ec7d4de5bfc82a119af9bb6aee8d0a27e5f
Jakir Hossain <83057318+jakir-hossain-web@users.noreply.github.com>
2023-07-18 21:21:30 +0600

    update

--- config/app.php
+++ config/app.php
@@ -182,6 +182,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
         Jorenvh\Share\Providers\ShareServiceProvider::class,
 
@@ -217,6 +218,7 @@ return [
         'Image' => Intervention\Image\Facades\Image::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
         'Share' => Jorenvh\Share\ShareFacade::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ])->toArray(),
 
 ];
